7e2ecdd1be9d176e94d629835fec4dab7e4cbf4a
KEYUR PATEL <82540200+Keyur2311@users.noreply.github.com>
2021-08-29 10:30:34 +0530

    Add files via upload

---
+++ SINGLY_LINKEDLIST_IMPLEMENTATION.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(Node *head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+
+    return;
+}
+
+void insertAtHead(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
+void display(Node *&head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << "";
+        if (temp->next != NULL)
+        {
+            cout << "->";
+        }
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+
+    insertAtTail(head, 10);
+    insertAtTail(head, 20);
+    insertAtTail(head, 30);
+
+    insertAtHead(head, 11);
+
+    // <----- SINGLY LINKEDLIST IMPLEMENTATION ----->
+
+    // TIME COMPLEXITY OF INSERTION :- O(N)
+
+    display(head);
+}
+
